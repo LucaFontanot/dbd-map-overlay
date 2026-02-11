9b5aa450b3ed218876386edad602fd6404494207
Luca <admin@lucaservers.com>
2026-02-12 00:48:33 +0100

    feat: Added rotation option hotkey

--- src/map/renderer.js
+++ src/map/renderer.js
@@ -1,26 +1,26 @@
-const { ipcRenderer } = require('electron');
+const {ipcRenderer} = require('electron');
 let url = null;
-ipcRenderer.on('map-change', async (event, img, size,opacity,draggable,rotation) => {
-    if (url!==null){
+ipcRenderer.on('map-change', async (event, img, size, opacity, draggable, rotation) => {
+    if (url !== null) {
         URL.revokeObjectURL(url)
     }
-    let imgData = Buffer.from(img,"base64");
+    let imgData = Buffer.from(img, "base64");
     let blob = new Blob([imgData]);
     url = URL.createObjectURL(blob);
-    $("#mainImg").attr("src",url).css({
-        "width":size+"px",
+    $("#mainImg").attr("src", url).css({
+        "width": size + "px",
         "opacity": opacity,
         "transform": `rotate(${rotation || 0}deg)`
     })
-    if (draggable){
-        $("body").css("-webkit-app-region","drag");
-    }else{
-        $("body").css("-webkit-app-region","no-drag");
+    if (draggable) {
+        $("body").css("-webkit-app-region", "drag");
+    } else {
+        $("body").css("-webkit-app-region", "no-drag");
     }
 
 });
 ipcRenderer.on('map-hide', async (event) => {
     $("#mainImg").css({
-        "width":"0px",
+        "width": "0px",
     })
 });
