3977e888c5bcbcc8f63bada0a9c845decce759cc
LucaFontanot <136001177+LucaFontanot@users.noreply.github.com>
2026-02-20 22:46:05 +0000

    Add generated files

--- src/js/consts.js
+++
@@ -1,4 +0,0 @@
-module.exports.BASEURL = "https://dbdmap.lucaservers.com"
-module.exports.GITHUB_ASSETS = "https://raw.githubusercontent.com/LucaFontanot/dbd-map-overlay"
-module.exports.ASSETS_REPO = "refs/heads/generated-pictures"
-module.exports.GITHUB_REPO = "master"
